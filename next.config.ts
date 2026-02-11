ef42ab618e4de57fce901cc2c441a57e64401405
smileluck <12386904+smileluck@users.noreply.github.com>
2026-02-11 13:27:33 +0800

    ci: 添加 Netlify 配置并更新 Next.js 配置
    添加 netlify.toml 配置文件用于 Netlify 部署
    更新 next.config.ts 以支持 Netlify 部署并添加新的开发域名

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['http://localhost:3000'],
+  allowedDevOrigins: ['http://localhost:3000', "http://fund.zsmile.top"],
 };
 
 export default nextConfig;
